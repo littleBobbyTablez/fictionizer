e0292b4000421ac4363eeb9d2ff9124be0166ad4
littleBobbyTabelz <jonas.st@mailbox.org>
2023-09-06 22:01:15 +0200

    add new projects page

--- main.go
+++ main.go
@@ -17,10 +17,20 @@ func main() {
         c.HTML(http.StatusOK, "index.html", gin.H{})
     })
 
+    r.POST("/project", func(c *gin.Context) {
+        name := c.PostForm("name")
+
+        c.HTML(http.StatusOK, "newProject.html", gin.H{ "name": name })
+    })
+
+    r.GET("/project", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "project.html", gin.H{})
+    })
+
     r.StaticFile("/output.css", "./statics/css/output.css")
 
-    fmt.Printf("Starting server at port 8080\n")
-    if err := r.Run(":8080"); err != nil {
+    fmt.Printf("Starting server at port 8089\n")
+    if err := r.Run(":8089"); err != nil {
         log.Fatal(err)
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./statics/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
